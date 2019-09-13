a60118ab6108814852b5a51b8a43512748fd3039
Peter Milne <peter.milne@d-tacq.com>
2019-09-13 11:49:15 +0100

    trim results to sensible precision, add PASS/FAIL 	modified:   usr/local/bin/web_cal_data.tcl

--- usr/local/bin/web_cal_data.tcl
+++ usr/local/bin/web_cal_data.tcl
@@ -10,26 +10,35 @@ set i0 {}
 set q0 {}
 set sens {}
 set tau {}
+set status {}
 
 set formatted_data [lsearch -all -inline -not -exact $formatted_data "calibration_data"]
 #set formatted_data [lreplace $formatted_data $idx $idx]
 
 
 for {set i 0} {$i <= [  llength $formatted_data ]} {incr i 5} {
-        lappend ch [ lindex $formatted_data $i ]
-        lappend i0 [ lindex $formatted_data [ expr $i+1 ] ]
-        lappend q0 [ lindex $formatted_data [ expr $i+2 ] ]
-        lappend sens [ lindex $formatted_data [ expr $i+3 ] ]
-        lappend tau  [ lindex $formatted_data [ expr $i+4 ] ]
-
+	catch {
+        	lappend ch [ lindex $formatted_data $i ]
+	        lappend i0   [format {%.3f} [ lindex $formatted_data [ expr $i+1 ] ] ]
+        	lappend q0   [format {%.3f} [ lindex $formatted_data [ expr $i+2 ] ] ]
+		set _sens    [format {%.3f} [ lindex $formatted_data [ expr $i+3 ] ] ]
+	        lappend sens $_sens
+	        lappend tau  [format {%.3f} [ lindex $formatted_data [ expr $i+4 ] ] ]
+		if { $_sens >= 1 && $_sens <= 10 } {
+			lappend status "PASS"
+		} else {
+			lappend status "FAIL"
+		}
+	}
 }
 
-set formatStr {%15s%20s%20s%17s%17s}
-puts [format $formatStr "Channel" "i0" "q0" "Sensitivity" "Cooling time" ]
+set formatStr {%15s%20s%20s%17s%17s%17s}
+puts [format $formatStr "Channel" "i0" "q0" "Sensitivity" "Cooling time" "Status" ]
 
-puts [format $formatStr "---------------" "-------------------" "-------------------" "---------------" "---------------"]
-foreach ch $ch i0 $i0 q0 $q0 sens $sens tau $tau {
-    puts [format $formatStr $ch $i0 $q0 $sens $tau]
+puts [format $formatStr "---------------" "-------------------" "-------------------" "---------------" "---------------" "-----------------"]
+
+foreach ch $ch i0 $i0 q0 $q0 sens $sens tau $tau status $status {
+    puts [format $formatStr $ch $i0 $q0 $sens $tau $status]
 }
 
 
